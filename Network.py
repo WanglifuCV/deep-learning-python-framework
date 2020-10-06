1aa08e502831adb9680808245e4d434fd8712e9b
andrew.wlf <andrew.wlf@antgroup.com>
2020-10-06 12:51:46 +0800

    初始版本的代码。前馈神经网络的雏形。有如下的内容需要修改： 1. 前向传播过程可以考虑和反向传播过程复用； 2. 网络描述可以考虑更加复杂一些。

--- Network.py
+++ Network.py
@@ -7,21 +7,35 @@ class Network(object):
     def __init__(self, layer_sizes):
         """
         列表layer_sizes包含了每一层对应神经元的个数。如果列表是[2, 3, 1]， 那么就是一个3层神经网络。
-        :param layer_sizes:
+        :param layer_sizes: 每一层的隐藏层的的宽度
         """
         self.layer_sizes = layer_sizes
         self.num_layers = len(self.layer_sizes)
         # 全用矩阵的尺寸来写
         # 目前self.weights和self.bias都是list， 以后可以考虑优化为dict或者ordereddict?
+        """
+        初始化工作放在这里。最开始的初始化用了np.random.random()，是[0,1]之间的均匀分布，导致加权之后的z一直是正的，
+        经过映射之后就进入饱和区，权重无法更新。因此初始化是非常重要的。
+        """
         self.weights = [np.random.randn(s1, s2) for s1, s2 in zip(layer_sizes[1:], layer_sizes[:-1])]
         self.bias = [np.random.randn(s, 1) for s in layer_sizes[1:]]
 
     def feed_forward(self, inputs):
+        """
+        前向传播
+        """
+        # TODO: 这一部分和反向传播时的前向推理可以考虑复用
         for W, b in zip(self.weights, self.bias):
             inputs = sigmoid(np.dot(W, inputs) + b)
         return inputs
 
     def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        """
+        利用随机梯度下降算法进行网络学习。
+        学习过程包含如下几部分：
+        1. 更新权重
+        2. 每个epoch测试精度。
+        """
         # 获得training_data的数据量
         training_data = list(training_data)
         training_data_num = len(training_data)
@@ -42,10 +56,9 @@ class Network(object):
 
     def update_mini_batch(self, mini_batch_data, eta):
         """
-        通过这个来更新权重
-        :param mini_batch_data:
-        :param eta:
-        :return:
+        权重更新模块。在这里利用反向传播来计算各个权重的梯度，然后更新权重。
+        :param mini_batch_data: 每个mini_batch
+        :param eta: 学习率
         """
         nabla_W = [np.zeros(weights.shape) for weights in self.weights]
         nabla_b = [np.zeros(bias.shape) for bias in self.bias]
@@ -69,6 +82,9 @@ class Network(object):
         return sum(int(x == y) for (x, y) in test_results)
 
     def summary(self):
+        """
+        展示各个层的宽度和尺寸
+        """
         print('Input layer, input size {}'.format(self.weights[0].shape[-1]))
         for idx, (W, b) in enumerate(zip(self.weights, self.bias)):
             print('Layer {}, Weights size {}, bias size {}.'.format(idx + 1, W.shape, b.shape))
